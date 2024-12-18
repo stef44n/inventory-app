f785e88a78684d324965f876ad664725843de012
Stefan <stefanseifeddine@gmail.com>
2024-12-18 18:18:32 +0000

    Update pool

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,9 @@ const { Pool } = require("pg");
 // All of the following properties should be read from environment variables
 // We're hardcoding them here for simplicity
 module.exports = new Pool({
-    host: "localhost", // or wherever the db is hosted
+    host: process.env.HOST || "localhost", // or wherever the db is hosted
     user: process.env.ROLE_NAME,
     database: process.env.DATABASE,
     password: process.env.ROLE_PASSWORD,
-    port: 5432, // The default port
+    port: process.env.PORT || 5432, // The default port
 });
